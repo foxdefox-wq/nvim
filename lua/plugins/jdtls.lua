0d79d0d10df281f7f1d10b350fe2546f003fcd5c
foxdefox-wq <bluefoxbrownbear@gmail.com>
2026-05-05 07:39:46 -0700

    e

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -1,14 +1,13 @@
--- ~/.config/nvim/lua/plugins/jdtls.lua
 return {
   "mfussenegger/nvim-jdtls",
   opts = function(_, opts)
     local is_windows = vim.fn.has("win32") == 1
 
-    if is_windows then
-      opts.settings = opts.settings or {}
-      opts.settings.java = opts.settings.java or {}
-      opts.settings.java.configuration = opts.settings.java.configuration or {}
+    opts.settings = opts.settings or {}
+    opts.settings.java = opts.settings.java or {}
+    opts.settings.java.configuration = opts.settings.java.configuration or {}
 
+    if is_windows then
       opts.settings.java.configuration.runtimes = {
         {
           name = "JavaSE-1.8",
@@ -22,6 +21,22 @@ return {
         {
           name = "JavaSE-21",
           path = "C:/Program Files/Eclipse Adoptium/jdk-21.0.11.10-hotspot",
+        },
+      }
+    else
+      opts.settings.java.configuration.runtimes = {
+        {
+          name = "JavaSE-1.8",
+          path = vim.fn.expand("~/.sdkman/candidates/java/8.0.482-tem"),
+        },
+        {
+          name = "JavaSE-25",
+          path = vim.fn.expand("~/.sdkman/candidates/java/25.0.3-tem"),
+          default = true,
+        },
+        {
+          name = "JavaSE-21",
+          path = vim.fn.expand("~/.sdkman/candidates/java/21.0.11-tem"),
         },
       }
     end
