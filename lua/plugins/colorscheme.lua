170c5ab82c6b901946e1069e556c1e2f145f692e
foxdefox-wq <bluefoxbrownbear@gmail.com>
2026-05-05 14:06:15 -0700

    e

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,9 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false, -- Load this during startup
-    priority = 1000, -- Load this before other plugins
-    opts = {
-      theme = "wave", -- Options: "wave", "dragon", "lotus"
-      background = {
-        dark = "wave", -- "wave" is the best for that professional IDE look
-      },
-    },
-    config = function(_, opts)
-      require("kanagawa").setup(opts)
-      vim.cmd("colorscheme kanagawa")
-    end,
+  "tinted-theming/tinted-nvim",
+  priority = 1000, -- load colorscheme early
+  lazy = false, -- apply on startup
+  opts = {
+    default_scheme = "base24-papercolor-dark", -- pick any bundled Base16/Base24
+    compile = true, -- optional: precompile for faster startup
   },
 }
