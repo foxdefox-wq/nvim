cac3becd4f58c0175e271240e058f46070e5b5d6
fox <bluefoxbrownbear@gmail.com>
2026-06-05 22:21:22 -0700

    E

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Minimal config with basic qol
+
+deps :
+c compiler, rg blah blah essentials, only thing out of the ordinary is argon which you need for the luau lsp server to work properly and u also need it on robox studio
--- lua/plugins/luau.lua
+++ lua/plugins/luau.lua
@@ -1,14 +1,29 @@
 return {
   "lopi-py/luau-lsp.nvim",
   config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == "luau-lsp" then
+          local root = client.root_dir
+          vim.fn.jobstart({ "argon", "serve", "--sourcemap" }, { cwd = root })
+        end
+      end,
+      once = true,
+    })
     require("luau-lsp").setup({
       platform = { type = "roblox" },
       sourcemap = {
-        enabled = false,
+        enabled = true,
         autogenerate = false,
+        generator_cmd = { "argon", "sourcemap", "--watch" },
       },
-      plugin = { enabled = true, port = 3667 },
+      plugin = { enabled = false },
       types = { roblox_security_level = "PluginSecurity" },
+      fflags = {
+        enable_new_solver = true,
+        sync = true,
+      },
     })
   end,
 }
